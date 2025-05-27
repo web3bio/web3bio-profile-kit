340fbcf7377039e0d327d45081dc853e9459e9cf
Yan Zhu <picturepan2@hotmail.com>
2025-05-27 10:07:07 +0700

    Update version and docs

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,7 @@
 export { useProfile } from "./useProfile";
 export { useNS } from "./useNS";
-export { useDomain } from "./useDomain";
 export { useUniversalProfile } from "./useUniversalProfile";
 export { useUniversalNS } from "./useUniversalNS";
-export { useBatchNS } from "./useBatchNS";
 export { useBatchProfile } from "./useBatchProfile";
+export { useBatchNS } from "./useBatchNS";
+export { useDomain } from "./useDomain";
