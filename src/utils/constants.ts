ad79e6af9320d8206a0c25a902a087c7f5597647
Yan Zhu <picturepan2@hotmail.com>
2025-05-29 00:36:16 +0700

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -19,25 +19,25 @@ export enum QueryEndpoint {
 
 // Regular expressions for identity detection
 export const REGEX = {
-  ENS: /^.+\.(eth|xyz|bio|app|luxe|kred|art|ceo|club|box)$/i,
-  BASENAMES: /^.+\.base(\.eth)?$/i,
-  LINEA: /^.+\.linea(\.eth)?$/i,
+  ENS: /^.+\.(?:eth|xyz|bio|app|luxe|kred|art|ceo|club|box|uni\.eth|cb\.id)$/i,
+  BASENAMES: /^.+\.base(?:\.eth)?$/i,
+  LINEA: /^.+\.linea(?:\.eth)?$/i,
   FARCASTER:
-    /^(?:[A-Za-z0-9_-]{1,61}(?:(?:\.eth)?(?:\.farcaster|\.fcast\.id|\.farcaster\.eth)?)?|farcaster,#\d+)$/i,
-  LENS: /^(?:.+\.lens)$/i,
+    /^(?:[A-Za-z0-9_-]{1,61}(?:\.(?:eth|farcaster|fcast\.id|farcaster\.eth))?|farcaster,#\d+)$/i,
+  LENS: /^.+\.lens$/i,
   CLUSTER: /^[\w-]+\/[\w-]+$/,
-  SPACE_ID: /^.+\.(bnb|arb)$/i,
+  SPACE_ID: /^.+\.(?:bnb|arb)$/i,
   GENOME: /^.+\.gno$/i,
   UNSTOPPABLE_DOMAINS:
-    /^.+\.(crypto|888|nft|blockchain|bitcoin|dao|x|klever|hi|zil|kresus|polygon|wallet|binanceus|anime|go|manga|eth)$/i,
+    /^.+\.(?:crypto|888|nft|blockchain|bitcoin|dao|x|klever|hi|zil|kresus|polygon|wallet|binanceus|anime|go|manga|eth)$/i,
   CROSSBELL: /^.+\.csb$/i,
   DOTBIT: /^.+\.bit$/i,
   SNS: /^.+\.sol$/i,
-  ETH_ADDRESS: /^0x[a-fA-F0-9]{40}$/i,
-  BTC_ADDRESS: /\b([13][a-km-zA-HJ-NP-Z1-9]{25,34}|bc1[qp][a-z0-9]{11,71})\b/,
+  ETH_ADDRESS: /^0x[a-fA-F0-9]{40}$/,
+  BTC_ADDRESS: /^(?:[13][a-km-zA-HJ-NP-Z1-9]{25,34}|bc1[qp][a-z0-9]{11,71})$/,
   SOLANA_ADDRESS: /^[1-9A-HJ-NP-Za-km-z]{32,44}$/,
   LOWERCASE_EXEMPT:
-    /\b(?:(?:[13][a-km-zA-HJ-NP-Z1-9]{25,34}|bc1[qp][a-z0-9]{11,71})|(?:[1-9A-HJ-NP-Za-km-z]{32,44}))\b/,
+    /^(?:[13][a-km-zA-HJ-NP-Z1-9]{25,34}|bc1[qp][a-z0-9]{11,71}|[1-9A-HJ-NP-Za-km-z]{32,44})$/,
   TWITTER: /^[A-Za-z0-9_]{1,15}(?:\.twitter)?$/i,
   NEXT_ID: /^0x[a-f0-9]{66}(?:\.nextid)?$/i,
 };
