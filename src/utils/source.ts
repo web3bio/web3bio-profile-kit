5521a83cb7a0fdbd99c7ca5325d4b414fc931db5
Yan Zhu <picturepan2@hotmail.com>
2025-08-23 21:43:11 +0700

    Update

--- src/utils/source.ts
+++ src/utils/source.ts
@@ -4,57 +4,113 @@ import { Source, type SourceType } from "../types/source";
  * @public
  */
 export const SOURCE_DATA: Readonly<Record<Source, SourceType>> = {
-  [Source.ethereum]: {
-    name: "Ethereum",
-    description: "Ethereum",
+  [Source.basenames]: {
+    name: "Basenames",
+    description: "The domain system on Base",
   },
-  [Source.ens]: {
-    name: "ENS",
-    description: "Ethereum Name Service",
+  [Source.camp_network]: {
+    name: "Camp Network",
+    description: "Modernize intellectual property infrastructure",
   },
-  [Source.twitter]: {
-    name: "Twitter (X)",
-    description: "Twitter (X) social platform",
+  [Source.clusters]: {
+    name: "Clusters",
+    description: "On-chain social protocol",
   },
-  [Source.keybase]: {
-    name: "Keybase",
-    description: "Secure messaging and file-sharing",
+  [Source.crowdsourcing]: {
+    name: "Crowdsourcing",
+    description: "Crowdsourcing",
   },
-  [Source.nextid]: {
-    name: "Next.ID",
-    description: "Decentralized identity protocol",
-  },
-  [Source.rss3]: {
-    name: "RSS3",
-    description: "Open information syndication protocol",
+  [Source.dentity]: {
+    name: "Dentity",
+    description: "Digital Credentials",
   },
   [Source.dotbit]: {
     name: ".bit",
     description: "Decentralized cross-chain identity system",
   },
-  [Source.unstoppabledomains]: {
-    name: "Unstoppable Domains",
-    description: "Blockchain domain name provider",
+  [Source.ens]: {
+    name: "ENS",
+    description: "Ethereum Name Service",
   },
-  [Source.lens]: {
-    name: "Lens",
-    description: "Web3 social graph protocol",
+  [Source.ethereum]: {
+    name: "Ethereum",
+    description: "Ethereum",
   },
   [Source.farcaster]: {
     name: "Farcaster",
     description: "Decentralized social network protocol",
   },
-  [Source.space_id]: {
-    name: "SpaceID",
-    description: "Multi-chain name service",
+  [Source.firefly]: {
+    name: "Firefly",
+    description: "Web3 social platform",
   },
-  [Source.gravity]: {
-    name: "Gravity",
-    description: "Gravity alpha mainnet name service",
+  [Source.foundation]: {
+    name: "Foundation",
+    description: "NFT marketplace",
   },
-  [Source.clusters]: {
-    name: "Clusters",
-    description: "On-chain social protocol",
+  [Source.gallery]: {
+    name: "Gallery",
+    description: "Show your collection to the world",
+  },
+  [Source.gmgn]: {
+    name: "GMGN",
+    description: "Web3 social platform",
+  },
+  [Source.icebreaker]: {
+    name: "Icebreaker",
+    description: "Open professional network",
+  },
+  [Source.justaname]: {
+    name: "JustaName",
+    description: "Identity infrastructure",
+  },
+  [Source.keybase]: {
+    name: "Keybase",
+    description: "Secure messaging and file-sharing",
+  },
+  [Source.lens]: {
+    name: "Lens",
+    description: "Web3 social graph protocol",
+  },
+  [Source.linea]: {
+    name: "Linea",
+    description: "L2 based on ZK",
+  },
+  [Source.mirror]: {
+    name: "Mirror",
+    description: "Decentralized publishing protocol",
+  },
+  [Source.nextid]: {
+    name: "Next.ID",
+    description: "Decentralized identity protocol",
+  },
+  [Source.nftd]: {
+    name: "NF.TD",
+    description: "Be your own checkmark",
+  },
+  [Source.nostr]: {
+    name: "Nostr",
+    description: "Web3 social platform",
+  },
+  [Source.opensea]: {
+    name: "OpenSea",
+    description: "NFT marketplace",
+  },
+  [Source.paragraph]: {
+    name: "Paragraph",
+    description: "Decentralized publishing protocol",
+  },
+  [Source.particle]: {
+    name: "Particle",
+    description: "Particle",
+  },
+  [Source.rarible]: {
+    name: "Rarible",
+    description: "NFT marketplace",
+  },
+  [Source.rss3]: {
+    name: "RSS3",
+    description: "Open information syndication protocol",
   },
   [Source.solana]: {
     name: "Solana",
@@ -64,77 +120,33 @@ export const SOURCE_DATA: Readonly<Record<Source, SourceType>> = {
     name: "SNS",
     description: "Solana Name Service",
   },
-  [Source.opensea]: {
-    name: "OpenSea",
-    description: "NFT marketplace",
-  },
-  [Source.firefly]: {
-    name: "Firefly",
-    description: "Web3 social platform",
-  },
-  [Source.basenames]: {
-    name: "Basenames",
-    description: "The domain system on Base",
-  },
-  [Source.dentity]: {
-    name: "Dentity",
-    description: "Digital Credentials",
-  },
-  [Source.nftd]: {
-    name: "NF.TD",
-    description: "Be your own checkmark",
-  },
-  [Source.mirror]: {
-    name: "Mirror",
-    description: "Decentralized publishing protocol",
-  },
-  [Source.paragraph]: {
-    name: "Paragraph",
-    description: "Decentralized publishing protocol",
-  },
-  [Source.foundation]: {
-    name: "Foundation",
-    description: "NFT marketplace",
-  },
-  [Source.rarible]: {
-    name: "Rarible",
-    description: "NFT marketplace",
-  },
   [Source.soundxyz]: {
     name: "Sound.xyz",
     description: "Decentralized audio platform",
   },
-  [Source.linea]: {
-    name: "Linea",
-    description: "L2 based on ZK",
-  },
-  [Source.gmgn]: {
-    name: "GMGN",
-    description: "Web3 social platform",
-  },
-  [Source.nostr]: {
-    name: "Nostr",
-    description: "Web3 social platform",
+  [Source.space_id]: {
+    name: "SpaceID",
+    description: "Multi-chain name service",
   },
   [Source.talentprotocol]: {
     name: "Talent",
     description: "Decentralized onchain passport",
   },
-  [Source.crowdsourcing]: {
-    name: "Crowdsourcing",
-    description: "Crowdsourcing",
-  },
-  [Source.particle]: {
-    name: "Particle",
-    description: "Particle",
-  },
   [Source.tally]: {
     name: "Tally",
     description: "Launch, manage, and grow the value of your token",
   },
-  [Source.icebreaker]: {
-    name: "Icebreaker",
-    description: "Open professional network",
+  [Source.twitter]: {
+    name: "Twitter (X)",
+    description: "Twitter (X) social platform",
+  },
+  [Source.unstoppabledomains]: {
+    name: "Unstoppable Domains",
+    description: "Blockchain domain name provider",
+  },
+  [Source.zora]: {
+    name: "Zora",
+    description: "A social network where every post is a coin",
   },
 };
 /**
