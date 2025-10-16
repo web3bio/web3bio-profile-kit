895231e0b8c678e45e60f46985377b9945f9dd01
billy <Billy@mask.io>
2025-10-16 15:37:38 +0800

    chore: updates

--- src/utils/platform.ts
+++ src/utils/platform.ts
@@ -242,6 +242,15 @@ export const PLATFORM_DATA: ReadonlyMap<
     },
   ],
   [
+    Platform.ethos,
+    {
+      color: "#232320",
+      icon: "icons/icon-ethos.svg",
+      label: "Ethos",
+      urlPrefix: "https://app.ethos.network/",
+    },
+  ],
+  [
     Platform.edgeless,
     {
       color: "#a0eb67",
