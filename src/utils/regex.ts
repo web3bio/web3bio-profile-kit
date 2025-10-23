9e6ed6fd9e5fc65fde0f898381f9be1aec6d9e62
Yan Zhu <picturepan2@hotmail.com>
2025-10-23 10:57:05 +0700

    Update domain regex

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -3,19 +3,19 @@
  * @public
  */
 export const REGEX = {
-  ENS: /^.+\.(?:eth|xyz|bio|app|luxe|kred|art|ceo|locker|club|box|uni\.eth|cb\.id|world\.id|gemini\.eth|int\.eth)$/i,
-  BASENAMES: /^.+\.base(\.eth)?$/i,
-  LINEA: /^.+\.linea(\.eth)?$/i,
+  ENS: /^[^.].+\.(?:eth|xyz|bio|app|luxe|kred|art|ceo|locker|club|box|id)$/i,
+  BASENAMES: /^[^.].+\.base(\.eth)?$/i,
+  LINEA: /^[^.].+\.linea(\.eth)?$/i,
   FARCASTER:
     /^(?:[A-Za-z0-9_-]{1,61}(?:\.(?:eth|farcaster|fcast\.id|farcaster\.eth))?|farcaster,#\d+)$/i,
-  LENS: /^.+\.lens$/i,
+  LENS: /^[^.].+\.lens$/i,
+  SNS: /^[^.].+\.sol$/i,
   CLUSTER: /^[\w-]+\/[\w-]+$/,
-  SPACE_ID: /^.+\.(?:bnb|arb)$/i,
-  SEEKERID: /^.+\.skr$/i,
+  SPACE_ID: /^[^.].+\.(?:bnb|arb)$/i,
+  SEEKERID: /^[^.].+\.skr$/i,
   UNSTOPPABLE_DOMAINS:
-    /^.+\.(?:crypto|888|nft|blockchain|bitcoin|dao|x|klever|hi|zil|kresus|polygon|wallet|binanceus|anime|go|manga|eth)$/i,
-  DOTBIT: /^.+\.bit$/i,
-  SNS: /^.+\.sol$/i,
+    /^[^.].+\.(?:crypto|888|nft|blockchain|bitcoin|dao|x|klever|hi|zil|kresus|polygon|wallet|binanceus|anime|go|manga|eth)$/i,
+  DOTBIT: /^[^.].+\.bit$/i,
   ETH_ADDRESS: /^0x[a-fA-F0-9]{40}$/i,
   BTC_ADDRESS: /^(?:[13][a-km-zA-HJ-NP-Z1-9]{25,34}|bc1[qp][a-z0-9]{11,71})$/,
   SOLANA_ADDRESS: /^[1-9A-HJ-NP-Za-km-z]{32,44}$/,
