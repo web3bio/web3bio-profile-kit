ce95b90b78b5a98dbdbe738870bf938f8b1f10e5
billy <Billy@mask.io>
2025-11-28 17:14:11 +0800

    chore: updates

--- src/utils/resolver.ts
+++ src/utils/resolver.ts
@@ -1,7 +1,4 @@
-import { METADATA_API_ENDPOINT } from "./helpers";
 import { isIPFS_Resource, resolveIPFS_URL } from "./ipfs";
-import { getNetwork } from "./network";
-import { REGEX } from "./regex";
 
 const URL_RESOLVERS = new Map([
   ["data:", (url: string) => url],
@@ -23,32 +20,4 @@ export const resolveMediaURL = (url: string): string => {
   }
 
   return url;
-};
-
-export const resolveEipAssetURL = async (
-  source: string,
-): Promise<string | null> => {
-  if (!source) return null;
-  const match = source.match(REGEX.EIP);
-  if (!match) return resolveMediaURL(source);
-  const [, chainId, , contractAddress, tokenId] = match;
-  if (!contractAddress || !tokenId) {
-    return resolveMediaURL(source);
-  }
-
-  const network = getNetwork(Number(chainId))?.key;
-
-  if (!network) {
-    return resolveMediaURL(source);
-  }
-
-  const fetchURL = `${METADATA_API_ENDPOINT}/api/nft/nft/${network}.${contractAddress}.${tokenId}`;
-  try {
-    const response = await fetch(fetchURL);
-    if (!response.ok) return null;
-    const data = await response.json();
-    return data?.image_url || null;
-  } catch {
-    return null;
-  }
 };
